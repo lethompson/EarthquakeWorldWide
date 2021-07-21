0e57c94e516db8fde616da07aee30b8bb2a1c35e
lethompson <lethompson1024@gmail.com>
2021-07-20 20:20:34 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$server     = "awseduinstance.ccq1ha3xaabo.us-west-1.rds.amazonaws.com";//RDS Endpoint (California)
-$username   = "Admin";//username
-$password   = "205cuPA!";//RDS password
-$db         = "opsworksdb"; //RDS DBname
+$server     = "";//RDS Endpoint (California)
+$username   = "";//username
+$password   = "";//RDS password
+$db         = ""; //RDS DBname
 
 // Create a connection
 $conn = mysqli_connect( $server, $username, $password, $db );
